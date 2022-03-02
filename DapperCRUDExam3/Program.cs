56cf3be67e0b89ca4af48c764823bc5cc3820e6f
Clever Coder <92303840+clevercoderr@users.noreply.github.com>
2022-03-02 10:10:32 +0500

    tugadi

--- DapperCRUDExam3/Program.cs
+++ DapperCRUDExam3/Program.cs
@@ -10,18 +10,32 @@ namespace DapperCRUDExam3
         static async Task Main(string[] args)
         {
             MusicService music = new MusicService();
+
             //await music.AddAsync(); //done
+            //PlaylistServices playlist = new PlaylistServices();
+            //var result = await playlist.GetAllAsync();
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item.Name);
+            //}
+
+            SingerServices singer = new SingerServices();
+            var rests = await singer.GetAllAsync();
+            foreach (var item in rests)
+            {
+                Console.WriteLine(item.FullName);
+            }
 
             //var getResult = await music.GetAsync(); done
 
             //Console.WriteLine(getResult.SingerId);
             //await music.Delete(2); done
-            /*var rests = await music.GetAllAsync(); //done
+            //var rests = await music.GetAllAsync(); //done
 
-            foreach (var rest in rests)
-            {
-                Console.WriteLine(rest.Id + " " + rest.Description);
-            }*/
+            //foreach (var rest in rests)
+            //{
+            //    Console.WriteLine(rest.Id + " " + rest.Description);
+            //}
             // await music.UpdateAsync("Bu yangi description", 1); done
 
 
